442bb809360437f54fea50c5274065dfc59d683c
Denisio <denisio.rodrigues@lynxspa.com>
2020-07-27 19:44:07 -0300

    Atendendo requisições diferentes

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -10,7 +11,25 @@ namespace Alura.ListaLeitura.App
     {
         public void ConfigureProduction(IApplicationBuilder app)
         {
-            app.Run(LivrosParaLer);
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento(HttpContext context) 
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, string>()
+            {
+                { "/Livros/ParaLer", _repo.ParaLer.ToString() },
+                { "/Livros/Lendo", _repo.Lendo.ToString() },
+                { "/Livros/Lidos", _repo.Lidos.ToString() }
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+
+            return context.Response.WriteAsync("Caminho inexistente.");
         }
 
         public Task LivrosParaLer(HttpContext context)
